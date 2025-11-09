cd0ffe171194206dd59a5e5cbbe6fa96d31bacd6
JeffryACh <jeffry2811ach@gmail.com>
2025-11-09 17:02:26 -0600

    Funcion de mas

--- Proyecto03A/Proyecto03A/main.cpp
+++ Proyecto03A/Proyecto03A/main.cpp
@@ -121,59 +121,6 @@ void decode(Node* raiz, int& index, string str)
 }
 
 /*
-* Crea el arbol de Huffman y decodifocas dados los simbolos
-* Observación: Construye el árbol de Huffman a partir de la frecuencia de los símbolos en el texto.
-* @param:
-*	- string text: Texto a comprimir y descomprimir.
-* @return:
-*	+ void: No retorna valor.
-*/
-void CrearArbol(string text) 
-{
-	// Cuenta la cantidad de veces que aparece cada simbolo y la guarda en el mapa
-	unordered_map<char, int> frecuencia;
-	for (char simbolo : text) 
-		frecuencia[simbolo]++;
-	
-	// Crea una cola de prioridad para guardar los nodos del arbol
-	priority_queue<Node*, vector<Node*>, comp> pq;
-	// Agrega a la cola de prioridad los nodos de cada simbolo
-	for (auto pair : frecuencia) 
-		pq.push(getNode(pair.first, pair.second, nullptr, nullptr));
-
-	// Hacer todo el proceso hasta que haya mas de 1 nodo en la cola
-	while (pq.size() != 1) 
-	{
-		Node* izq = pq.top(); pq.pop();
-		Node* der = pq.top(); pq.pop();
-		int sum = izq->frecuencia + der->frecuencia;
-		pq.push(getNode('\0', sum, izq, der));
-	}
-
-	Node* raiz = pq.top();
-	// Imprime los codigos para cada simbolo
-	unordered_map<char, string> CodigoHuffman;
-	encode(raiz, "", CodigoHuffman);
-	cout << "\nLos codigos de Huffman son:\n" << "\n";
-	for (auto pair : CodigoHuffman) 
-		cout << pair.first << " " << pair.second << '\n';
-
-	// Imprime el mensaje original que el usuario ingreso directamente, el codificado
-	// y decodifica el codificado y lo imprime de nuevo
-	cout << "\nEl mensaje original era:\n" << text << '\n';
-	string str = "";
-	for (char simbolo : text) 
-		str += CodigoHuffman[simbolo];
-	
-	cout << "\nEl texto codificado es: \n" << str << '\n';
-	int index = -1;
-	cout << "\nEl texto decodificado es: \n";
-	while (index < (int)str.size() - 1)
-		decode(raiz, index, str);
-	cout << "\n\n\n";
-}
-
-/*
 * Crea el arbol de Huffman y comprime un archivo
 * Observación: Lee un archivo, construye el árbol de Huffman y crea un archivo comprimido.
 * @param:
@@ -300,6 +247,14 @@ void DescomprimirArchivo(const string& inputFile, const string& outputFile)
 	cout << "Archivo descomprimido correctamente: " << outputFile << endl;
 }
 
+/*
+* Convierte una cadena de texto a minusculas
+* Observación: Utiliza la función transform para convertir todos los caracteres de una cadena a minúsculas.
+* @param:
+*	- string& texto: Cadena de texto a convertir.
+* @return:
+*	+ string: Cadena de texto convertida a minúsculas.
+*/
 string convertirMinusculas(string& texto) 
 {
 	transform(texto.begin(), texto.end(), texto.begin(), [](unsigned char c) {
